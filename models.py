6fee1f74735cbdb622d477d9ae770852c5d589c1
alikamranli <kamranlil324@gmail.com>
2022-08-17 22:22:56 +0400

    flask

--- models.py
+++ models.py
@@ -1,15 +1,16 @@
 from run import db,app
 
-class Products (db.Model):
+class Products(db.Model):
     id=db.Column(db.Integer,primary_key=True,autoincrement=True)
-    productName=db.Column(db.Integer)
-    is_active=db.Column(db.Boolean,default=True)
-    add_date=db.Column(db.DateTime)
+    brand=db.Column(db.String(100))
+    model=db.Column(db.String(100))
+    description=db.Column(db.String(200))
+    
 
-class Orders (db.Model):
+class Orders(db.Model):
     id=db.Column(db. Integer,primary_key=True,autoincrement=True)
     order_date=db.Column(db.DateTime)
-    order_status=db.Column(db.String(100), )
+    order_status=db.Column(db.String(100))
     order_total=db.Column(db.Integer, )
     is_active=db.Column(db.Boolean, default=True)
     add_date=db.Column(db.DateTime)
--- routes/admin/product.py
+++ routes/admin/product.py
@@ -1,24 +1,30 @@
 #products routes routes/product.py
 
 
+
 from flask import redirect,render_template,request,url_for
 from run import app
 
 @app.route('/add',methods=['GET','POST'])
 def add():
     from models import Products,db
-    if request.method=="POST":
+    if request.method=='POST':
         brand=request.form['u_brand']
         model=request.form['u_model']
         description=request.form['u_description']
         prdct=Products(brand=brand,model=model,description=description)
         db.session.add(prdct)
         db.session.commit()
-        return redirect('/add')
+        return redirect('/products')
 
-    return render_template("product/list.html")
+    return render_template("admin/product/add.html")
 
-
+@app.route('/products',methods=['GET','POST'])
+def products():
+    from models import Products,db
+    products=Products.query.all()
+    return render_template('admin/product/list.html',products=products)
+    
     
 @app.route('/delete/<id>',methods=['GET','POST'])
 def delete(id):
@@ -27,17 +33,19 @@ def delete(id):
     db.session.delete(prodct)
   
     db.session.commit()
-    return redirect('/list')
+    return redirect('/products')
 
 @app.route('/update/<id>',methods=['GET','POST'])
 def update(id):
     from models import Products,db
     product=Products.query.get(id)
     if request.method=='POST':
-        brand=request.form['u_brand']
-        model=request.form['u_model']
-        description=request.form['u_description']
+
+        product.brand=request.form['u_brand']
+        product.model=request.form['u_model']
+        product.description=request.form['u_description']
         db.session.commit()
         return redirect('/products')
-    return render_template("update.html",description=description)
+    return render_template("admin/product/update.html",product=
+    product)
 
